c54109f4177f8fc53e00ea23410d4c9116e0240b
Dia Pal <diap76046@gmail.com>
2025-08-22 12:00:00 +0530

    Add files via upload

---
+++ 01_problem1.c
@@ -0,0 +1,14 @@
+/* 
+Q1 (User Inputs, Operations & Output)
+Write a program to input two numbers and display their sum.
+*/
+ #include<stdio.h>
+ 
+ int main(){
+    int a,b;
+    a=b = 4;
+    int sum = a+b ;
+    printf("The  sum will be : %d ", sum);
+    
+    return 0;
+ }
