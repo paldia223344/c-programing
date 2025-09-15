5966268d12c63c80dbc166e28fe7f5e6b203e208
Dia Pal <diap76046@gmail.com>
2025-09-15 10:44:26 +0530

    Add files via upload

---
+++ 52_problem52.c
@@ -0,0 +1,63 @@
+/*
+Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+
+ #include<stdio.h>
+ 
+ int main(){
+    int i;
+    for ( i = 0; i < 1; i++)
+    {
+        printf("*\n");
+    }
+     printf("\n");
+
+     for ( i = 0; i < 3; i++)
+    {
+        printf("*\n");
+    }
+     printf("\n");
+
+     for ( i = 0; i < 5; i++)
+    {
+        printf("*\n");
+    }
+     printf("\n");
+
+     for ( i = 0; i < 3; i++)
+    {
+        printf("*\n");
+    }
+     printf("\n");
+
+     for ( i = 1; i < 1; i++)
+    {
+        printf("*\n");
+    }
+     printf("\n");
+
+    
+    
+    
+    
+    
+    return 0;
+ }
