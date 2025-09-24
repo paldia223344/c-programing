8ac96bb9ed9bb4a3786e7d6ee0b8cf845336fbe7
Dia Pal <diap76046@gmail.com>
2025-09-24 23:59:55 +0530

    Add files via upload

---
+++ 70_problem70.c
@@ -0,0 +1,34 @@
+/*
+Q70: Rotate an array to the right by k positions.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+4 5 1 2 3
+
+*/
+ #include<stdio.h>
+ 
+ int main(){
+    int arr[10] = {1,2,3,4,5};
+    int i, n, value , value1;
+    printf("At what position do we rotate");
+    scanf("%d", &n);
+       for ( i = 4-n; i < 4; i++)
+       {
+        printf("%d", arr[i]);
+       }
+       
+       for ( i = 0; i < 4-k; i++)
+       {
+         printf("%d", arr[i]);
+       }
+       
+
+        
+    return 0;
+ }
